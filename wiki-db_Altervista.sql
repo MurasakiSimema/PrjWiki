2dfb5fe8af8e6d8e4c9fe450ab4b37ca149d1ecb
Simone Bergonzi <simemablak@gmail.com>
2021-01-19 19:17:32 +0100

    Aggiunto DB altervista

--- wiki-db_Altervista.sql
+++ wiki-db_Altervista.sql
@@ -10,20 +10,7 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `wiki`
---
-CREATE DATABASE IF NOT EXISTS `wiki` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `wiki`;
-
--- --------------------------------------------------------
+USE my_hoilserveracasa
 
 --
 -- Struttura della tabella `lingue`
